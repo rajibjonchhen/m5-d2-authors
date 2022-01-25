36da386a6382eed6633050663947caed389dd635
rajibjonchhen <rajib.jonchhen@gmail.com>
2022-01-25 16:11:48 +0000

    npm i cors added

--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -1,10 +1,22 @@
 import express from "express";
+import res from "express/lib/response";
 
 const authorsRouter = express.Router()
 
-authorsRouter.post("/",()=>{})
+// for creating
+authorsRouter.post("/",(req,res)=>{
+    res.send('hello')
+})
+
+// for getting the list
 authorsRouter.get("/",()=>{})
+
+// for getting  the individual items
 authorsRouter.get("/:authorId",()=>{})
+
+// for editing the information
 authorsRouter.put("/:authorId",()=>{})
+
+// for deleting the items
 authorsRouter.delete("/:authorId",()=>{})
 export default  authorsRouter
